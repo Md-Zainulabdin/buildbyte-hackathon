d7db6ab251a1a947690d652e7a9debc256c60581
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2026-07-18 17:43:09 +0500

    feat: post opportunity page with skills, location, category, paid/volunteer

---
+++ apps/web/src/constants/categories.ts
@@ -0,0 +1,11 @@
+export const CATEGORIES = [
+  "Teaching & Tutoring",
+  "Design & Creative",
+  "Web & Tech",
+  "Events & Photography",
+  "Health & Wellness",
+  "Legal & Consulting",
+  "Writing & Translation",
+  "Community Service",
+  "Other",
+] as const;
